f5f8d5c6eafd82e09863c2016d92cd8265214f8d
Mufan Qiu <mufan@cs.unc.edu>
2024-11-26 16:03:47 -0500

    logging + train loop

--- f1_direct_regression.py
+++ f1_direct_regression.py
@@ -1,9 +1,17 @@
+from calendar import c
+from math import e
+import yaml
 import torch
 import torch.nn as nn
 
 from tqdm import tqdm
+from util.logger import ExperimentLogger
 from datasets.sapphire import SapphireDataset
 from torch.utils.data import DataLoader
+
+CONFIG_FP = (
+    "/playpen/mufan/levi/tianlong-chen-lab/material-super-resolution/config.yaml"
+)
 
 
 class RegressionHead(nn.Module):
@@ -19,7 +27,19 @@ class RegressionHead(nn.Module):
         return self.fc1(x)
 
 
+def parse_config(fp: str) -> dict:
+    with open(fp, "r") as f:
+        config = yaml.safe_load(f)
+    return config
+
+
 def main():
+
+    config = parse_config(CONFIG_FP)
+    logger = ExperimentLogger(config_fp=CONFIG_FP, exp_name="f1_direct_regression")
+    logger.add_result_columns(
+        ["global_train_step", "global_val_step", "epoch", "train_loss", "val_loss"]
+    )
 
     # create model + change classification head
     model = torch.hub.load("pytorch/vision:v0.10.0", "resnet152", pretrained=True)
@@ -29,16 +49,24 @@ def main():
     model.fc = RegressionHead(in_features)
 
     # create train/val dataset and dataloader
-    train_dataset = SapphireDataset(split="train")
-    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False)
-    val_dataset = SapphireDataset(split="val")
-    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+    train_dataset = SapphireDataset(
+        split="train", steps_per_epoch=config["training"]["steps_per_epoch"]
+    )
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=config["training"]["batch_size"], shuffle=False
+    )
+    val_dataset = SapphireDataset(
+        split="val", steps_per_epoch=config["validation"]["steps_per_epoch"]
+    )
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=config["validation"]["batch_size"], shuffle=False
+    )
 
     # Define the loss function and optimizer
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=float(config["training"]["lr"]))
 
-    num_epochs = 10
+    num_epochs = config["training"]["epochs"]
     device = 4
     model = model.to(device)
 
@@ -47,24 +75,30 @@ def main():
         model.train()  # Set model to training mode
         running_loss = 0.0
 
-        for batch in tqdm(train_dataloader, desc="Batches"):
-            # Move inputs and targets to device
+        for i, batch in enumerate(tqdm(train_dataloader, desc="Batches")):
+
             X = batch["X"].to(device)
             z = batch["z"].to(device)
-            # Zero the parameter gradients
             optimizer.zero_grad()
-            # Forward pass
+            # forward
             outputs = model(X)
-            # Ensure the target has the correct shape
+            # TODO: do we need this?
             if z.dim() == 1:
                 z = z.unsqueeze(1)
-            # Compute loss
+
             loss = criterion(outputs, z)
-            # Backward pass and optimization
             loss.backward()
             optimizer.step()
-            # Accumulate running loss
             running_loss += loss.item() * X.size(0)
+            logger.log(
+                **{
+                    "global_train_step": len(train_dataloader) * (epoch) + i,
+                    "global_val_step": None,
+                    "epoch": epoch,
+                    "train_loss": loss.item(),
+                    "val_loss": None,
+                }
+            )
 
         # Compute average loss for the epoch
         epoch_loss = running_loss / len(train_dataset)
@@ -74,7 +108,7 @@ def main():
         model.eval()  # Set model to evaluation mode
         val_running_loss = 0.0
         with torch.no_grad():  # Disable gradient computation
-            for batch in tqdm(val_dataloader, desc="Batches"):
+            for i, batch in enumerate(tqdm(val_dataloader, desc="Batches")):
                 # Move inputs and targets to device
                 X = batch["X"].to(device)
                 z = batch["z"].to(device)
@@ -87,6 +121,15 @@ def main():
                 loss = criterion(outputs, z)
                 # Accumulate validation loss
                 val_running_loss += loss.item() * X.size(0)
+                logger.log(
+                    **{
+                        "global_train_step": None,
+                        "global_val_step": len(val_dataloader) * (epoch) + i,
+                        "epoch": epoch,
+                        "train_loss": None,
+                        "val_loss": loss.item(),
+                    }
+                )
 
         # Compute average validation loss
         val_loss = val_running_loss / len(val_dataset)
--- util/logger.py
+++ util/logger.py
@@ -1,188 +1,73 @@
 import math
 import os
 import csv
-import numpy as np
 import torch
+import yaml
+import datetime
+import numpy as np
+import pandas as pd
 import torch.nn.functional as F
-import torchvision
 
 from typing import List, Dict, Optional
-from metrics import calc_psnr
-from cv2 import log
-from PIL import Image
-from pytorch_lightning.callbacks import Callback
-from pytorch_lightning.utilities.distributed import rank_zero_only
 
-LOG_FP = "/playpen/mufan/levi/tianlong-chen-lab/material-super-resolution/ControlNet/__runs__/11-19-24-64x64-crop/log.csv"
+EXPS_DIR = "/playpen/mufan/levi/tianlong-chen-lab/material-super-resolution/__exps__"
 
-# TODO: create a custom callback
-# 1. MSE train loss @ each step
-# 2. MSE (i.e., pixel-loss) validation loss @ each epoch
-# 3. PSNR @ each epoch
 
-# TODO: implement
-class Logger:
-    def __init__(self) -> None:
-        pass
+# TODO: implement an experiment launcher for easy, co-ordinated ablations
+class ExperimentLauncher:
+    pass
 
-class ScuffedLogger:
+
+class ExperimentLogger:
     """
-    Singleton logger.
+    A flexible logger used for recording and organizing experimental runs.
     """
-    
-    _instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(ScuffedLogger, cls).__new__(cls)
-        return cls._instance
-
-    def __init__(self, log_out_path: Optional[str] = None) -> None:
-        if hasattr(self, 'initialized') and self.initialized:
-            return
-
-        if log_out_path is not None:
-            self.csv_out_path = log_out_path
-        else:
-            self.csv_out_path = LOG_FP
-        
-        self.HEADERS = ['Step', 'Epoch', 'Train Loss', 'Val Loss', 'Val MSE', 'Val PSNR']
-
-        self.steps: List[int] = []
-        self.epochs: List[int] = []
-        self.train_losses: List[float] = []
-        self.val_losses: List[float] = []
-        self.val_mse: List[float] = []
-        self.val_psnr: List[float] = []
-
-        self.curr_step = 0
-        self.curr_epoch = 0
-        self.curr_train_loss = 0.0
-        self.curr_val_loss = 0.0
-        self.curr_mse = 0.0
-        self.curr_psnr = 0.0
-        self.initialized = True
-
-    @classmethod
-    def get_instance(cls):
+    def __init__(self, config_fp: str, exp_name: Optional[str] = "") -> None:
         """
-        Returns the singleton instance of ScuffedLogger.
-        If the instance doesn't exist, it creates one with the given log_out_path.
-        Subsequent calls ignore the log_out_path parameter.
+
+        :param config_fp: path to a `.yaml` config file containing all hps
         """
-        if cls._instance is None:
-            cls._instance = cls
-        return cls._instance
+        assert config_fp.endswith(".yaml")
+        self.config_fp = config_fp
+        self.exp_name = exp_name
+        self.results_out_path: Optional[str] = None
+        self.results = pd.DataFrame()
+        self._setup_exp_dir()
 
-    def update_reconstruction_error(self, mse: float, psnr: float):
-        self.curr_mse = mse
-        self.curr_psnr = psnr
-        self.curr_epoch += 1
-        self.refresh_csv()
+    def _update_csv(self):
+        self.results.to_csv(self.results_out_path, index=False)
 
-    def update_losess(self, loss, loss_dict: Dict):
-        self.curr_step += 1
-        if 'val/loss' in loss_dict.keys(): 
-            self.curr_val_loss = loss
-        else:
-            self.curr_train_loss = loss
-        self.refresh_csv()
+    def _setup_exp_dir(self):
 
-    def refresh_csv(self):
-        self.steps.append(self.curr_step)
-        self.epochs.append(self.curr_epoch)
-        self.train_losses.append(self.curr_train_loss)
-        self.val_losses.append(self.curr_val_loss)
-        self.val_mse.append(self.curr_mse)
-        self.val_psnr.append(self.curr_psnr)
+        # get date and time as a string
+        date_time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        subdir_name = date_time_str + "_" + self.exp_name
+        exp_out_dir = os.path.join(EXPS_DIR, subdir_name)
 
-        # make the output dir if needed
-        out_dir = os.path.dirname(self.csv_out_path)
-        os.makedirs(out_dir, exist_ok=True)
-        
-        # write to CSV
-        with open(self.csv_out_path, mode='w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(self.HEADERS)
-            writer.writerows(zip(self.steps, self.epochs, self.train_losses, self.val_losses, self.val_mse, self.val_psnr))
+        # make new subdir if needed
+        os.makedirs(exp_out_dir, exist_ok=True)
+        # save config in subdir
+        config_save_fp = os.path.join(exp_out_dir, "config.yaml")
+        with open(config_save_fp, "w") as f:
+            with open(self.config_fp, "r") as g:
+                f.write(g.read())
+        self.config_fp = config_save_fp
 
-class ImageLogger(Callback):
-    def __init__(self, batch_frequency=2000, max_images=4, clamp=True, increase_log_steps=True,
-                 rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
-                 log_images_kwargs=None):
-        super().__init__()
-        self.rescale = rescale
-        self.batch_freq = batch_frequency
-        self.max_images = max_images
-        if not increase_log_steps:
-            self.log_steps = [self.batch_freq]
-        self.clamp = clamp
-        self.disabled = disabled
-        self.log_on_batch_idx = log_on_batch_idx
-        self.log_images_kwargs = log_images_kwargs if log_images_kwargs else {}
-        self.log_first_step = log_first_step
-        
-        # image logger now has an instance of ScuffedLogger
-        self.scuffed_logger = ScuffedLogger.get_instance()
+        # path to results csv file
+        self.results_out_path = os.path.join(exp_out_dir, "results.csv")
 
-    @rank_zero_only
-    def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
-        # breakpoint()
-        root = os.path.join(save_dir, "image_log", split)
-        for k in images:
-            grid = torchvision.utils.make_grid(images[k], nrow=4)
-            if self.rescale:
-                grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
-            grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
-            grid = grid.numpy()
-            grid = (grid * 255).astype(np.uint8)
-            filename = "{}_gs-{:06}_e-{:06}_b-{:06}.png".format(k, global_step, current_epoch, batch_idx)
-            path = os.path.join(root, filename)
-            os.makedirs(os.path.split(path)[0], exist_ok=True)
-            Image.fromarray(grid).save(path)
-            
-        # (B, C, H, W ) -> (3, 512, 512)
-        # HACK: we assume a BS of 1 for logging
-        input: torch.Tensor = images['conditioning'].squeeze(0).detach().cpu()
-        target: torch.Tensor = images['reconstruction'].squeeze(0).detach().cpu()
-        
-        mse = F.mse_loss(input, target).item()
-        psnr = calc_psnr(input, target)
-        
-        self.scuffed_logger.update_reconstruction_error(mse, psnr)
-        
-    def log_img(self, pl_module, batch, batch_idx, split="train"):
-        check_idx = batch_idx  # if self.log_on_batch_idx else pl_module.global_step
-        if (self.check_frequency(check_idx) and  # batch_idx % self.batch_freq == 0
-                hasattr(pl_module, "log_images") and
-                callable(pl_module.log_images) and
-                self.max_images > 0):
-            logger = type(pl_module.logger)
+    def add_result_column(self, name: str):
+        self.results[name] = None
+        self._update_csv()
 
-            is_train = pl_module.training
-            if is_train:
-                pl_module.eval()
+    def add_result_columns(self, names: List[str]):
+        for name in names:
+            self.add_result_column(name)
+        self._update_csv()
 
-            with torch.no_grad():
-                images = pl_module.log_images(batch, split=split, **self.log_images_kwargs)
-
-            for k in images:
-                N = min(images[k].shape[0], self.max_images)
-                images[k] = images[k][:N]
-                if isinstance(images[k], torch.Tensor):
-                    images[k] = images[k].detach().cpu()
-                    if self.clamp:
-                        images[k] = torch.clamp(images[k], -1., 1.)
-
-            self.log_local(pl_module.logger.save_dir, split, images,
-                           pl_module.global_step, pl_module.current_epoch, batch_idx)
-
-            if is_train:
-                pl_module.train()
-
-    def check_frequency(self, check_idx):
-        return check_idx % self.batch_freq == 0
-
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        if not self.disabled:
-            self.log_img(pl_module, batch, batch_idx, split="train")
+    def log(self, **kwargs):
+        self.results = pd.concat(
+            [self.results, pd.DataFrame.from_records([kwargs])], ignore_index=True
+        )
+        self._update_csv()
